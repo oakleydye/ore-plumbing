719031af7dd504d7aa915ec480e8dbbfe279307d
Oakley Dye <oakley.dye@gmail.com>
2026-03-05 22:32:54 -0700

    Fix gtag implementation

--- src/app/(ore-plumbing)/layout.tsx
+++ src/app/(ore-plumbing)/layout.tsx
@@ -43,17 +43,20 @@ export default function RootLayout({
       >
         {/* Google Ads (gtag) */}
         <Script
+          async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-          `}
-        </Script>
+        <Script id="google-analytics" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: 
+            `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+            `
+        }}
+        />
 
         <Providers>
           <Analytics />
